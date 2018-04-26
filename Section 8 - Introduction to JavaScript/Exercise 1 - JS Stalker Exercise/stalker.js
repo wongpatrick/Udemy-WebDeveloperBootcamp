8dd438af09844259c8649c41f274a6524cfc3483
Patrick Wong <wongpatrick280@gmail.com>
2018-04-26 16:57:55 -0400

    Javascript exercise

---
+++ Section 8 - Introduction to JavaScript/Exercise 1 - JS Stalker Exercise/stalker.js
@@ -0,0 +1,6 @@
+var firstname = prompt("What is your first name?")
+var lastname = prompt("What is your last name?")
+var age = prompt("What is your age?")
+
+console.log("Your full name is "+ firstname + " " + lastname)
+console.log("Your age is "+ age)
