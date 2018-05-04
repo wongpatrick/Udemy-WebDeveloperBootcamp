7144054770939404b82243369c8c8d94b95b0856
Patrick Wong <wongpatrick280@gmail.com>
2018-05-04 18:01:38 -0400

    finished exercise

---
+++ Section 9 - Javascript Basics - Control Flow/While Loop Problem Set/whileExercise.js
@@ -0,0 +1,39 @@
+
+console.log("print all numbers between -10 and 19")
+var number = -10;
+ while (number < 20 )
+ {
+ 	console.log(number)
+ 	number++
+ }
+
+console.log("print all numbers between -10 and 19")
+number = 10;
+ while (number < 41)
+ {
+ 	if (number % 2 == 0) {
+ 		console.log(number)
+ 	}
+ 	number++
+ }
+
+ console.log("print all odd numbers between 300 and 333")
+ number = 300
+ while (number < 334)
+ {
+ 	if (number % 2 != 0)
+ 	{
+ 		console.log(number)
+ 	}
+ 	number ++;
+ }
+
+ console.log("print all odd numbers between 5 and 50")
+ number = 5
+ while (number < 51)
+ {
+ 	if (number % 5 == 0 && number % 3 == 0) {
+ 		console.log(number);
+ 	}
+ 	number++
+ }
