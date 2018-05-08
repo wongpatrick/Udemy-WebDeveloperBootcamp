9982658dd952ec238f8f9c59b4d9722020bba17e
Patrick Wong <wongpatrick280@gmail.com>
2018-05-08 06:32:21 +0000

    added edit/update

--- Section 29 - RESTful Routing/BlogApp/app.js
+++ Section 29 - RESTful Routing/BlogApp/app.js
@@ -1,13 +1,15 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override");
     
 // APP config
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // Mongoose/model config
 var blogSchema = new mongoose.Schema({
@@ -66,6 +68,27 @@ app.get("/blogs/:id", function(req,res){
         }
     });
 });
+
+app.get("/blogs/:id/edit", function(req,res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err) {
+            res.redirect("/blogs");
+            console.log(err)
+        } else {
+            res.render("edit",{blog: foundBlog});
+        }
+    });
+});
+
+app.put("/blogs/:id", function(req,res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err,updatedBlog){
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    })
+})
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING!");
