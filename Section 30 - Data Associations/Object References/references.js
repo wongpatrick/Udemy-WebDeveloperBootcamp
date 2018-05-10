f17d7258596a1fb1b935811b9260c9b3772adabb
Patrick Wong <wongpatrick280@gmail.com>
2018-05-10 04:41:36 +0000

    finished section 30

---
+++ Section 30 - Data Associations/Object References/references.js
@@ -0,0 +1,62 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/blog_demo_2");
+
+
+// POST - title, content
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+var Post = mongoose.model("Post", postSchema);
+
+// USER - email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }]
+});
+
+var User = mongoose.model("User", userSchema);
+
+//find user
+// find all posts for that user
+
+User.findOne({email:"bob@gmail.com"}).populate("posts").exec(function(err,user){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(user);
+    }
+});
+
+
+// Post.create({
+//     title: "How to cook the best burger pt. 3",
+//     content: "asdfkjklsdajflkajlsdf"
+// }, function(err, post){
+//     User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             foundUser.posts.push(post);
+//             foundUser.save(function(err,data){
+//               if (err) {
+//                   console.log(err)
+//               } else {
+//                   console.log(data);
+//               }
+//             });
+//         }
+//     });
+// });
+
+
+// User.create({
+//     email:"bob@gmail.com",
+//     name: "Bob Belcher"
+// });
+
