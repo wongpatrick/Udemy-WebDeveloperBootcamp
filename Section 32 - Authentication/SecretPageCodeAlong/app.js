7ed8d8f397846a7f9d2699ed5582db11fbd569cc
Patrick Wong <wongpatrick280@gmail.com>
2018-05-17 05:29:50 +0000

    Finished section 32

--- Section 32 - Authentication/SecretPageCodeAlong/app.js
+++ Section 32 - Authentication/SecretPageCodeAlong/app.js
@@ -10,8 +10,9 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
    
 
 var app = express();
-app.use(passport.initialize());
-app.use(passport.session());
+
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(require("express-session")({
     secret: "Rusty is the best and cutest dog in the world",
@@ -19,18 +20,71 @@ app.use(require("express-session")({
     saveUninitialized: false
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
     
+//========
+//ROUTES
+//========
 
 app.get("/", function(req,res){
     res.render("home");
 });
 
-app.get("/secret", function(req,res){
+app.get("/secret", isLoggedIn, function(req,res){
     res.render("secret");
 });
 
+// Auth Routes
+
+// Show sign up form
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+app.post("/register", function(req,res){
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username}), req.body.password, function(err,user){
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        } 
+        passport.authenticate("local")(req,res, function(){
+           res.redirect("/secret"); 
+        });
+    });
+});
+
+// Login Routes
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req,res){
+});
+
+// Logout Routes
+app.get("/logout", function(req,res){
+   req.logout();
+   res.redirect("/");
+});
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
 app.listen(process.env.PORT,process.env.IP, function(){
     console.log("Server has started");
-})
+});
